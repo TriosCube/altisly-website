62732616dbeb353a4d1f5718b5163ad8ed3fe87c
Felix Chima <drfelixilin@gmail.com>
2026-05-21 12:25:08 +0100

    analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', '@vercel/analytics/nuxt'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
